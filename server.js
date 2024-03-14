78ab59c55ce96c53207ff7b13bc51b73904b4670
DaniConstantinescu <mihai.constantinescu02@e-uvt.ro>
2024-03-14 17:15:43 +0200

    Update server.js
    Hosted to local network

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
 const express = require('express')
+const https = require('https');
+const fs = require('fs');
+
 const app = express()
 const port = 5000
 
@@ -10,10 +13,27 @@ const deviceRouter = require('./routes/devices')
 
 app.use(express.json());
 
+app.get('/test', (req, res) =>{
+    res.json({"message": "Hello"})
+})
+
 app.use('/hubs', hubRouter)
 app.use('/rooms', roomRouter)
 app.use('/scenes', sceneRouter)
 app.use('/schedules', scheduleRouter)
 app.use('/devices', deviceRouter)
 
-app.listen(port, () => console.log(`Server started on port ${port}`))
+
+app.listen(port, "0.0.0.0", () => console.log(`Server started on port ${port}`))
+
+// HTTPS
+// const options = {
+//     key: fs.readFileSync('certs/private.key', 'utf8'),
+//     cert: fs.readFileSync('certs/certificate.pem', 'utf8'),
+//     passphrase: 'parola' 
+// };
+
+//   const server = https.createServer(options, app);
+//   server.listen(443, '0.0.0.0', () => {
+//     console.log('Server running on https://0.0.0.0:443/');
+//   });
